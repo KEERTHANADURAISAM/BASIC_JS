a926f854c449cbf3f2648600aeb060c7691d48e9
KEERTHANADURAISAM <tarakeerthu@gmail.com>
2025-04-02 16:39:50 +0530

    add navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+    const navItems = ["Cart", "Sign In"];
+    return (
+        <nav className="navbar">
+            <h2 className="logo">MyStore</h2>
+            <ul className="nav-links">
+                {navItems.map((item, index) => (
+                    <li key={index} className="nav-item">{item}</li>
+                ))}
+            </ul>
+        </nav>
+    );
+};
 
-export default Navbar
+export default Navbar;
